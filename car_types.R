7714a85ea315495d506624314033a826a807292b
Atanas Ivanov <va.studio.central@gmail.com>
2025-04-10 07:55:41 +0300

    Messed up purpose of the script, fixed now

---
+++ car_types.R
@@ -0,0 +1,14 @@
+#Here we will see the numbers of cars with significant differences from each other
+hist(mtcars$am,
+     breaks = 2,
+     xlab = "Transmission type (0 - Automatic, 1 - Manuel)",
+     main = "Transmission type for the 32 automobiles (1973–74 models)
+     included int the 1974 Motor Trend US magazine.",
+     col = "#2980b9")
+
+hist(mtcars$vs,
+     breaks = 2,
+     xlab = "Engine type (0 - V-shaped, 1 - straight)",
+     main = "Engine type for the 32 automobiles (1973–74 models)
+     included int the 1974 Motor Trend US magazine.",
+     col = "#2980b9")
