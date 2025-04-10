83747bbe30e9b67393115e79138fbbdf17ff952e
Atanas Ivanov <va.studio.central@gmail.com>
2025-04-10 08:06:49 +0300

    Added variables for easier view of wwhat is going on

--- summary.R
+++ summary.R
@@ -5,47 +5,59 @@ data(mtcars)
 ?mtcars
 head(mtcars)
 
+#Variables for easier use
+FuelConsumption <- mtcars$mpg
+HorsePower <- mtcars$hp
+CarWeight <- mtcars$wt
+cyl_counts <- table(mtcars$cyl)
+
 #Basic graphs
 #Graphing a histogram to see what are the frequencies for each main variable in the data set
 
 #Miles per Gallon
-hist(mtcars$mpg, 
+hist(FuelConsumption, 
      breaks = 5,
      xlab = "Miles per Gallon",
+     ylab = "Count of cars which relate",
      main = "Miles per Gallon Frequencies, for 32 automobiles (1973–74 models)
      included int the 1974 Motor Trend US magazine.",
      col = "#2980b9"
      )
 
-rug(mtcars$mpg, lwd = 2, col = 'gray')
+rug(FuelConsumption, lwd = 2, col = '#2c3e50')
 
 #Horsepower
-hist(mtcars$hp,
+hist(HorsePower,
      breaks = 5,
      xlab = "HorsePower Range",
+     ylab = "Amount of cars which relate",
      main = "HorsePower Range Frequencies, for 32 automobiles (1973–74 models)
      included int the 1974 Motor Trend US magazine.",
      col = "#2980b9")
 
-rug(mtcars$hp, lwd = 2, col = "#2c3e50")
+rug(HorsePower, lwd = 2, col = "#2c3e50")
 
 #Weight
-hist(mtcars$wt,
+hist(CarWeight,
      breaks = 5,
      xlab = "Weight Range in TONS",
      main = "Weight Range Frequencies, for 32 automobiles (1973–74 models)
      included int the 1974 Motor Trend US magazine.",
      col = "#2980b9")
 
-rug(mtcars$wt, lwd = 2, col = "#2c3e50")
+rug(CarWeight, lwd = 2, col = "#2c3e50")
 
 #Number of Cylinders
-cyl_counts <- table(mtcars$cyl)
 labels <- paste(names(cyl_counts), "cyl -", round(100 * cyl_counts / sum(cyl_counts), 1), "%")
 pie(cyl_counts, 
     labels = labels,
     main = "Car Counts by Cylinder for 32 automobiles (1973–74 models)
      included int the 1974 Motor Trend US magazine.", 
     col = c("#3498db", "#2ecc71", "#e74c3c"))
+
+#Base information
+#Making Graphs to describe the range of specs for the cars included in the magazine
+
+
 
 
