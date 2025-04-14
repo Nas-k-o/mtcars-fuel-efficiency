7b03f038ae025c5b32e9c511f6222428814dc7e3
Atanas Ivanov <va.studio.central@gmail.com>
2025-04-14 09:18:07 +0300

    Experimenting with GGPLOT2, will redo all of the plots

--- MPG_Vs.R
+++ MPG_Vs.R
@@ -1,15 +1,16 @@
 #In this R script we will compare Miles Per Gallon variable against Horsepower, Weight and Number of Cylinders
 #Plotting MPG against HP
-plot(mtcars$hp, mtcars$mpg,
-     main = "Miles per Gallon against Horsepower",
-     ylab = "Miles per Gallon",
-     xlab = "Horsepower",
-     col = "steelblue",
-     pch = 19,
-     cex = 1.5)
+p <- ggplot(mtcars, aes(x = hp, y = mpg)) +
+  geom_point(color = "steelblue", lwd = 3) +  
+  geom_smooth(method = "lm", color = "red", size = 1.5, se = FALSE) +  
+  labs(
+    title = "Miles per Gallon against Horsepower",
+    x = "Horsepower",
+    y = "Miles per Gallon"
+  ) +
+  theme_minimal(base_size = 14)
 
-abline(lm(mtcars$mpg ~ mtcars$hp), col = "red", lwd = 2)
-
+print(p)
 
 #Plotting MPG against Weight
 plot(mtcars$wt, mtcars$mpg,
