c5115dd0213931d164ac67ae910ea251a06b8082
Atanas Ivanov <va.studio.central@gmail.com>
2025-04-16 08:24:10 +0300

    Using OS we can now specify which file to open

--- ExcelAnalytics.py
+++ ExcelAnalytics.py
@@ -2,5 +2,26 @@ import pandas as pd
 import os
 import openpyxl
 
-excelDF = pd.read_excel("ExcelTables/table_automaticCarsSummary.csv.xlsx", usecols="B:C")
-print(excelDF)
+def list_excel_files(directory="ExcelTables"):
+    files = [f for f in os.listdir(directory) if f.endswith(".xlsx")]
+    for idx, file in enumerate(files, start=1):
+        print(f"[{idx}] - {file}")
+    return files
+
+def select_xlsx():
+    global ExcelFile
+    files = list_excel_files()
+    choice = int(input("Select File: "))
+    if 1 <= choice <= len(files):
+        ExcelFile = os.path.join("ExcelTables", files[choice - 1])
+        loadTest(ExcelFile)
+    else:
+        print("Invalid selection.")
+
+
+def loadTest(excelfile):
+    excelDF = pd.read_excel(excelfile, usecols="B:C")
+    print(excelDF)
+
+list_excel_files()
+select_xlsx()
