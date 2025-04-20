5d0e363d9cc675c1cb3608bfa424d9f6a0a3cef7
Atanas Ivanov <va.studio.central@gmail.com>
2025-04-20 15:41:58 +0300

    User can now Select a car and it will create an object of it

--- CarsAnalytics.py
+++ CarsAnalytics.py
@@ -1,3 +1,4 @@
+from csv import excel
 from tkinter.font import names
 
 import pandas as pd
@@ -5,8 +6,31 @@ import os
 import openpyxl
 
 def Main():
+    global excelDF
     excelDF = pd.DataFrame(pd.read_excel("ExcelTables/table_cars.csv.xlsx"))
+    list_Cars()
 
+def list_Cars():
+    i = 1
+    for car in excelDF["Car"]:
+        print("[" + str(i) + "]" + " - " + car)
+        i += 1
+    i = int(input("Select: "))
+    SelectCar(i)
+
+def SelectCar(i):
+    car = Automobile()
+    car.name = excelDF.loc[i-1, "Car"]
+    car.mpg = excelDF.loc[i-1, "mpg"]
+    car.disp = excelDF.loc[i-1, "disp"]
+    car.hp = excelDF.loc[i - 1, "hp"]
+    car.drat = excelDF.loc[i - 1, "drat"]
+    car.qsec = excelDF.loc[i - 1, "qsec"]
+    car.vs = "V-shaped" if excelDF.loc[i - 1, "vs"] == 0 else "Straight"
+    car.am = "Automatic" if excelDF.loc[i - 1, "am"] == 0 else "Manual"
+    car.gear = excelDF.loc[i - 1, "gear"]
+    car.carb = excelDF.loc[i - 1, "carb"]
+    print(car.vs)
 
 class Automobile:
     def __init__(self):
