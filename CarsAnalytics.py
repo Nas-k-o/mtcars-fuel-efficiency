506455b669ae535465035f9e5485c63aeecdb096
Atanas Ivanov <va.studio.central@gmail.com>
2025-04-20 15:17:28 +0300

    First look at the __init__ of Automobile Class

--- CarsAnalytics.py
+++ CarsAnalytics.py
@@ -6,15 +6,19 @@ import openpyxl
 
 def Main():
     excelDF = pd.DataFrame(pd.read_excel("ExcelTables/table_cars.csv.xlsx"))
-    objCar = Automobile()
-    objCar.name = excelDF.loc[0, "Car"]
-    print(excelDF)
-    print(objCar.name)
 
 
 class Automobile:
     def __init__(self):
         self.name = ""
-        self.mpg = 0
+        self.mpg = 0.0
+        self.disp = 0.0
+        self.hp = 0
+        self.drat = 0
+        self.qsec = 0.0
+        self.vs = ""
+        self.am = ""
+        self.gear = 0
+        self.carb = 0
 
 Main()
