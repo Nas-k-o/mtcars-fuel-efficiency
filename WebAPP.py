3e49187f8cab5d0d212856f94a9a6a1ba5963521
Atanas Ivanov <va.studio.central@gmail.com>
2025-04-27 11:31:30 +0300

    Index.html is looking fine. Added cars.html which will be used for all of the functions from CarsAnalytics.py, Also rewrote the code from CarsAnalytics to WebAPP.py

--- WebAPP.py
+++ WebAPP.py
@@ -4,36 +4,82 @@ import os
 
 app = Flask(__name__)
 
-
-default_file = "table_cars.csv.xlsx"  # Set the default file name
-
-
-def list_excel_files(directory="ExcelTables"):
-    files = [f for f in os.listdir(directory) if f.endswith(".xlsx")]
-    return files
-
-
-def select_xlsx(file_choice=None):
-    if not file_choice:
-        file_choice = default_file
-
-    excel_file = os.path.join("ExcelTables", file_choice)
-    excelDF = pd.read_excel(excel_file)
-    return excelDF
-
+# Load data once, since it is being used multiple times
+excelDF = pd.read_excel("ExcelTables/table_cars.csv.xlsx")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     table_data = None
-
     if request.method == "POST":
-        excelDF = select_xlsx()
         table_data = excelDF.to_html(classes="data", header="true", index=False)
-    else:
-        excelDF = select_xlsx()
-        table_data = excelDF.to_html(classes="data", header="true", index=False)
-
     return render_template("index.html", table_data=table_data)
+
+
+@app.route("/cars", methods=["GET", "POST"])
+def cars():
+    if request.method == "POST":
+        selected_car_name = request.form.get("car")
+        selected_car = get_car_data(selected_car_name)
+
+        # Handling tuning inputs
+        if request.form.get("simulate"):
+            add_wt = float(request.form.get("add_weight"))
+            add_hp = int(request.form.get("add_hp"))
+            new_car_specs = simulate_tuning(selected_car, add_wt, add_hp)
+            return render_template("cars.html", car_data=selected_car, new_car_specs=new_car_specs)
+
+        # Handling comparison logic
+        if request.form.get("compare"):
+            compare_car_name = request.form.get("compare_car")
+            compare_car = get_car_data(compare_car_name)
+            comparison_data = compare_cars(selected_car, compare_car)
+            return render_template("cars.html", car_data=selected_car, comparison_data=comparison_data)
+
+    # Default car selection on GET
+    car_data = get_car_data(excelDF["Car"].iloc[0])  # Default to the first car
+    return render_template("cars.html", car_data=car_data)
+
+
+def get_car_data(car_name):
+    car_data = excelDF[excelDF["Car"] == car_name].iloc[0]
+    return {
+        "name": car_data["Car"],
+        "mpg": car_data["mpg"],
+        "hp": car_data["hp"],
+        "wt": car_data["wt"],
+        "qsec": car_data["qsec"]
+    }
+
+
+def simulate_tuning(car, add_wt, add_hp):
+    new_wt = car['wt'] + add_wt
+    new_hp = car['hp'] + add_hp
+    new_mpg = estimate_mpg(new_hp, new_wt)
+    new_qsec = estimate_qsec(new_hp, new_wt)
+
+    return {
+        'Weight': round(new_wt, 2),
+        'Horsepower': round(new_hp, 2),
+        'MPG': round(new_mpg, 2),
+        'QSEC': round(new_qsec, 2)
+    }
+
+
+def compare_cars(car1, car2):
+    return {
+        'Weight': [car1['wt'], car2['wt']],
+        'Horsepower': [car1['hp'], car2['hp']],
+        'MPG': [car1['mpg'], car2['mpg']],
+        'QSEC': [car1['qsec'], car2['qsec']]
+    }
+
+
+def estimate_qsec(hp, wt_tons):
+    return 6.5 * (wt_tons * 1000 / hp) ** 0.3
+
+
+def estimate_mpg(hp, wt_tons):
+    return 90 * (wt_tons / hp) ** 0.6
 
 
 if __name__ == "__main__":
