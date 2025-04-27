5cc1abae17245819e7dcc97c9a690d5c81366ac9
Atanas Ivanov <va.studio.central@gmail.com>
2025-04-27 09:43:18 +0300

    Added visualisation of cars table. Which will be showed in the sample data in index.html

--- WebAPP.py
+++ WebAPP.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
+import edit_summary as es
+import ExcelAnalytics as ea
+import CarsAnalytics as ca
+from ExcelAnalytics import list_excel_files, select_xlsx
 
 app = Flask(__name__)
 
-@app.route("/")
+default_file = "ExcelTables/table_cars.csv.xlsx"
+
+
+@app.route("/", methods = ["POST","GET"])
 def index():
-    return render_template("index.html")
+    files = list_excel_files()
+    table_data = None
+
+    excelDF = select_xlsx(default_file)
+    table_data = excelDF.to_html(classes="data", header="true", index=False)
+
+    return render_template("index.html", table_data=table_data)
+
+
 
 if __name__ == "__main__":
     app.run()
